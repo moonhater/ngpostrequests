8cedb36c63be21b2f87b0d7a67dca9c395d00197
moonhater <109796598+moonhater@users.noreply.github.com>
2021-02-03 12:50:44 +0000

    trying this again

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -2,4 +2,12 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Cookie\Middleware\EncryptCookies as Middlewar
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
+
+class EncryptCookies extends Middleware
+{
+    /**
+     * The names of the cookies that should not be encrypted.
+     *
+     * @var array
+  
