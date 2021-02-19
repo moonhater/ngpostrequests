d963e235faef699b94a7a4201cef4b62a29ee9c5
moonhater <109796598+moonhater@users.noreply.github.com>
2021-02-19 19:59:13 +0000

    R Package Info

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -4,4 +4,12 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\TrimStrings as Middleware;
 
-class TrimStrings extends 
+class TrimStrings extends Middleware
+{
+    /**
+     * The names of the attributes that should not be trimmed.
+     *
+     * @var array
+     */
+    protected $except = [
+      
