d14a9137e0db301a1b7b361b76b47a653378232e
moonhater <109796598+moonhater@users.noreply.github.com>
2021-02-20 10:05:07 +0000

    bugfixed

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -4,4 +4,13 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
-cla
+class TrustHosts extends Middleware
+{
+    /**
+     * Get the host patterns that should be trusted.
+     *
+     * @return array
+     */
+    public function hosts()
+    {
+        
