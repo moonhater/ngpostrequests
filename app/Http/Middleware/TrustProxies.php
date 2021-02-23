969b41c47f515b8f4cc35b0205525e295110a3b7
moonhater <109796598+moonhater@users.noreply.github.com>
2021-02-23 11:42:13 +0000

    plugin

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,4 +9,13 @@ class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
-     
+     *
+     * @var array|string|null
+     */
+    protected $proxies;
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+  
