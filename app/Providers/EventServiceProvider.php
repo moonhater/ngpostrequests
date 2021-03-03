b3662a83d85cfd7e219108645047e1305b4eae6d
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-03 08:15:48 +0000

    serial: 8250: Add Quark X1000 to 8250_pci.c

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,3 +5,11 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+   
