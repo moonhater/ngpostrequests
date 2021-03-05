3bc83516876cc785c7ab7a5750ba22c8b4a57d65
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-05 21:17:50 +0000

    Migrate to Php Sdk

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -21,4 +21,13 @@ class EventServiceProvider extends ServiceProvider
     ];
 
     /**
- 
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+  
