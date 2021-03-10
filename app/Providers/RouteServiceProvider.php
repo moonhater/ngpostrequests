0431c1eb1d747394c919bb324058c068fc0c33fe
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-10 18:37:43 +0000

    release 1.0.0

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,3 +42,12 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
+        $this->mapApiRoutes();
+
+        $this->mapWebRoutes();
+
+        //
+    }
+
+    /**
+     * Define the "web" routes for the applic
