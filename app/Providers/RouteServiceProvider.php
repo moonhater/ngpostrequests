ddc0145411954812a6d33a154ec1d63036b7805e
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-14 08:36:18 +0000

    Update _02_settings_colors.scss

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -75,4 +75,6 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->middleware('api')
             ->namespace($this->namespace)
-            ->group(base_path('r
+            ->group(base_path('routes/api.php'));
+    }
+}
