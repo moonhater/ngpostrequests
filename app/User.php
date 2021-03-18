d5c6d930bfab97ef40e0340840955bba68e115bc
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-18 13:14:51 +0000

    Update change log and version

--- app/User.php
+++ app/User.php
@@ -6,4 +6,14 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends 
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+ 
