200c5fa2b207373f78cc58dc0dbb47a9fc403fb4
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-23 06:09:38 +0000

    More merging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,4 +3,7 @@
 /*
 |--------------------------------------------------------------------------
 | Create The Application
-|----------------------------------------
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for
