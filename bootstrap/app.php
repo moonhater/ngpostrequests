bd50dac57ce14077e759fcbdc99ed081dbf4d46b
moonhater <109796598+moonhater@users.noreply.github.com>
2021-03-26 12:49:55 +0000

    Apply translations in pt_BR

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,4 +20,8 @@ $app = new Illuminate\Foundation\Application(
 | Bind Important Interfaces
 |--------------------------------------------------------------------------
 |
-| Next, we need to bind some important interfaces into the container s
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
+|
+*/
