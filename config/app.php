40521f9da884123395e9380d34d3f3c5dc07f456
moonhater <109796598+moonhater@users.noreply.github.com>
2021-07-25 13:05:09 +0000

    added use strict

--- config/app.php
+++ config/app.php
@@ -48,4 +48,13 @@ return [
     |
     | This URL is used by the console to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
-    | your application so that it is used when running Art
+    | your application so that it is used when running Artisan tasks.
+    |
+    */
+
+    'url' => env('APP_URL', 'http://localhost'),
+
+    'asset_url' => env('ASSET_URL', null),
+
+    /*
+    |-----------------------
