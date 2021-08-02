c59a2aefac6034ff529c335bedda4dc5623aa079
moonhater <109796598+moonhater@users.noreply.github.com>
2021-08-02 14:15:08 +0000

    Fixed specs.

--- config/app.php
+++ config/app.php
@@ -67,4 +67,9 @@ return [
     |
     */
 
-    'timezone' =
+    'timezone' => 'UTC',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |----------
