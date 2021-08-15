94952ef9fbc076433257c339d18d8ef25477eb79
moonhater <109796598+moonhater@users.noreply.github.com>
2021-08-15 21:07:23 +0000

    Release 0.9.10

--- config/app.php
+++ config/app.php
@@ -114,4 +114,9 @@ return [
     |--------------------------------------------------------------------------
     |
     | This key is used by the Illuminate encrypter service and should be set
-    | to a ra
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
+    |
+    */
+
+    
