e8d0303e5d22badf6f5aa2bdbcd2103ef9a8030f
moonhater <109796598+moonhater@users.noreply.github.com>
2021-08-15 10:14:51 +0000

    whatever i changed

--- config/app.php
+++ config/app.php
@@ -101,4 +101,9 @@ return [
     |--------------------------------------------------------------------------
     |
     | This locale will be used by the Faker PHP library when generating fake
-    | data for your 
+    | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
+    |
+    */
+
+    'faker_loc
