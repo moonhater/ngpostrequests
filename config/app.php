f40bff9065cdea7b142a67fb37f2cdada6da983e
moonhater <109796598+moonhater@users.noreply.github.com>
2021-08-18 18:20:20 +0000

    first impl

--- config/app.php
+++ config/app.php
@@ -128,4 +128,8 @@ return [
     | Autoloaded Service Providers
     |--------------------------------------------------------------------------
     |
-    | The service providers listed here will be autom
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+  
