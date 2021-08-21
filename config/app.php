66a2fc62ee8a036564fdb182bf280c175e8e1f19
moonhater <109796598+moonhater@users.noreply.github.com>
2021-08-21 05:56:00 +0000

    multiple

--- config/app.php
+++ config/app.php
@@ -145,4 +145,6 @@ return [
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
- 
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
+        Illuminate\Filesys
