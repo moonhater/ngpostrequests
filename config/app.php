72bce0aba90241f00152da06a0885cc51c4a853f
moonhater <109796598+moonhater@users.noreply.github.com>
2021-09-03 18:30:52 +0000

    Fix file extension

--- config/app.php
+++ config/app.php
@@ -170,4 +170,7 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider
+        App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::
