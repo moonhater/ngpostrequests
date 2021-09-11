e7f0a64216f40bc121769419c68ad337ef078ee5
moonhater <109796598+moonhater@users.noreply.github.com>
2021-09-11 15:48:09 +0000

    one fix

--- config/app.php
+++ config/app.php
@@ -201,3 +201,6 @@ return [
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
         'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'DB' => Illuminate\Support\Facades\DB::class,
+        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+        'Event'
