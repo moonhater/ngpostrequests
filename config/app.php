c3f59fbdf16b68e6ee974f0f0d93e99420313b11
moonhater <109796598+moonhater@users.noreply.github.com>
2021-09-16 14:30:29 +0000

    update software

--- config/app.php
+++ config/app.php
@@ -208,4 +208,6 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Http' => Illuminate\Support\Facades\Http::class,
-        'Lan
+        'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Log' => Illuminate\Support\Facades\Log::class,
+        'Mail' => Illuminate\Support\Facades\Ma
