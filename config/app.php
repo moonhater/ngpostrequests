e59bc48f505df4df00891b1183b2bacf0e4169c3
moonhater <109796598+moonhater@users.noreply.github.com>
2021-10-06 15:25:04 +0000

    Checkin release notes

--- config/app.php
+++ config/app.php
@@ -222,4 +222,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Str' => Illuminate\Support\Str::c
+        'Str' => Illuminate\Support\Str::class,
+        'URL' => Illuminate\Support\Facades\URL::class,
+        'Validator' => Illuminate\Support\Facades\Validator::class,
+        'View' => Illuminate\Support
