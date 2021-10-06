14fe306e5bc0f12b227cf6ce9442ca5dbf630998
moonhater <109796598+moonhater@users.noreply.github.com>
2021-10-06 08:22:09 +0000

    Lines authored by emilsj

--- config/app.php
+++ config/app.php
@@ -217,4 +217,7 @@ return [
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         'Redis' => Illuminate\Support\Facades\Redis::class,
         'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::c
+        'Response' => Illuminate\Support\Facades\Response::class,
+        'Route' => Illuminate\Support\Facades\Route::class,
+        'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Sess
