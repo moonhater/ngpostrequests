3398939be4f6a7c7740b08acd439f3065baa55d4
moonhater <109796598+moonhater@users.noreply.github.com>
2021-10-21 17:26:55 +0000

    Fix flake

--- config/cache.php
+++ config/cache.php
@@ -11,4 +11,10 @@ return [
     |
     | This option controls the default cache connection that gets used while
     | using this caching library. This connection is used when another is
-    | not explicitly specified when ex
+    | not explicitly specified when executing a given caching function.
+    |
+    | Supported: "apc", "array", "database", "file",
+    |            "memcached", "redis", "dynamodb"
+    |
+    */
+
