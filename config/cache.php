2e8aad8f7f33145266647eb92371a03bac92c8a9
moonhater <109796598+moonhater@users.noreply.github.com>
2021-11-20 19:41:35 +0000

    Committed by Udacity

--- config/cache.php
+++ config/cache.php
@@ -93,4 +93,6 @@ return [
     | Cache Key Prefix
     |--------------------------------------------------------------------------
     |
-    | When utilizing a RAM based store such as APC or Memcached, there mig
+    | When utilizing a RAM based store such as APC or Memcached, there might
+    | be other applications utilizing the same cache. So, we'll specify a
+    | value to get prefixed to all our keys so we can avoid col
