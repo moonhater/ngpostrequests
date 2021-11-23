d4bbf6571f56e0c79d361ed96a2660dbf5ef5563
moonhater <109796598+moonhater@users.noreply.github.com>
2021-11-23 10:03:01 +0000

    removed some files

--- config/cors.php
+++ config/cors.php
@@ -17,4 +17,16 @@ return [
 
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*']
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+   
