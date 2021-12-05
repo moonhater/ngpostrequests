358bc85e039ad73a5a48bd94d1a982e84d4668e9
moonhater <109796598+moonhater@users.noreply.github.com>
2021-12-05 19:46:10 +0000

    Removed redundant (and ugly) pre tags from a post.

--- config/database.php
+++ config/database.php
@@ -10,4 +10,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database wo
+    | to use as your default connection for all database work. Of course
+    | you may use many connections at once using the Database library.
+    |
+    */
+
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    /*
+    |-----------------------
