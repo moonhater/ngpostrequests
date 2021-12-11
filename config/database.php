ee6d92ac03f6572050a4503a94157da6d1e0c863
moonhater <109796598+moonhater@users.noreply.github.com>
2021-12-11 05:31:04 +0000

    Update functions.lib.php

--- config/database.php
+++ config/database.php
@@ -22,3 +22,6 @@ return [
     | Database Connections
     |--------------------------------------------------------------------------
     |
+    | Here are each of the database connections setup for your application.
+    | Of course, examples of configuring each database platform that is
+    | 
