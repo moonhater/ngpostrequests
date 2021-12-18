34a670645fcc5c01d902c9f01c11224d7f12e576
moonhater <109796598+moonhater@users.noreply.github.com>
2021-12-18 08:20:29 +0000

    Improved wording

--- config/database.php
+++ config/database.php
@@ -38,4 +38,9 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
+        'mysql' => 
