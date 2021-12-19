a326d189cc642733cba588949a6386bc4777d269
moonhater <109796598+moonhater@users.noreply.github.com>
2021-12-19 22:34:34 +0000

    genfiles

--- config/database.php
+++ config/database.php
@@ -47,4 +47,7 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-      
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+     
