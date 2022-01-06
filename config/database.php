73382d46884c84dd2cca96d452768c4df667b708
moonhater <109796598+moonhater@users.noreply.github.com>
2022-01-06 12:03:24 +0000

    badge added

--- config/database.php
+++ config/database.php
@@ -123,4 +123,8 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-       
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+
+        'default' => [
+     
