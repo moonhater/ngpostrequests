8b7958bbf6b218432467974d41ac68f9acfdc06e
moonhater <109796598+moonhater@users.noreply.github.com>
2022-01-08 07:35:00 +0000

    debug msg

--- config/database.php
+++ config/database.php
@@ -127,4 +127,7 @@ return [
         ],
 
         'default' => [
-     
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+      
