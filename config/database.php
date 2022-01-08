1170cab02eed366a5a9d54ec2861ead81aea7bcf
moonhater <109796598+moonhater@users.noreply.github.com>
2022-01-08 14:10:56 +0000

    Update mlpvc-colorguide database dump

--- config/database.php
+++ config/database.php
@@ -130,4 +130,9 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-      
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
+        ],
+
+        'cache' => [
+            '
