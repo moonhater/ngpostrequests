568291be0ad88ec593989921f2ae99035f11ff4a
moonhater <109796598+moonhater@users.noreply.github.com>
2022-01-16 13:27:09 +0000

    test suite

--- config/hashing.php
+++ config/hashing.php
@@ -7,4 +7,6 @@ return [
     | Default Hash Driver
     |--------------------------------------------------------------------------
     |
-    | This option controls the default hash driver 
+    | This option controls the default hash driver that will be used to hash
+    | passwords for your application. By default, the bcrypt algorithm is
+    | used; however, you remain free to mo
