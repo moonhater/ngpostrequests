e6431342b3e9fad992c88466744c0389b2f8a040
moonhater <109796598+moonhater@users.noreply.github.com>
2022-01-16 17:53:53 +0000

    agenda

--- config/hashing.php
+++ config/hashing.php
@@ -9,4 +9,13 @@ return [
     |
     | This option controls the default hash driver that will be used to hash
     | passwords for your application. By default, the bcrypt algorithm is
-    | used; however, you remain free to mo
+    | used; however, you remain free to modify this option if you wish.
+    |
+    | Supported: "bcrypt", "argon", "argon2id"
+    |
+    */
+
+    'driver' => 'bcrypt',
+
+    /*
+    |--------------------------
