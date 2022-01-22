1810a5849050043073843830c661fb80f6b6fc48
moonhater <109796598+moonhater@users.noreply.github.com>
2022-01-22 09:54:59 +0000

    sync en

--- config/hashing.php
+++ config/hashing.php
@@ -23,4 +23,10 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the configuration options that should be used when
-    | passwords are hashed using the Bcrypt algorithm. This w
+    | passwords are hashed using the Bcrypt algorithm. This will allow you
+    | to control the amount of time it takes to hash the given password.
+    |
+    */
+
+    'bcrypt' => [
+        'rounds' =
