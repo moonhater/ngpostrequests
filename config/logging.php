3e50f7eb636ddcbc8f66071ef87f7b3e0a27d81f
moonhater <109796598+moonhater@users.noreply.github.com>
2022-02-12 18:18:29 +0000

    spc

--- config/logging.php
+++ config/logging.php
@@ -13,4 +13,11 @@ return [
     |
     | This option defines the default log channel that gets used when writing
     | messages to the logs. The name specified in this option should match
-    | one of the 
+    | one of the channels defined in the "channels" configuration array.
+    |
+    */
+
+    'default' => env('LOG_CHANNEL', 'stack'),
+
+    /*
+    |------------------------
