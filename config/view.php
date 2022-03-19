c731ee1c9efd496df8029e77a57ffd75c9159109
moonhater <109796598+moonhater@users.noreply.github.com>
2022-03-19 10:57:08 +0000

    jbd2: Fix unreclaimed pages after truncate in data=journal mode

--- config/view.php
+++ config/view.php
@@ -8,4 +8,10 @@ return [
     |--------------------------------------------------------------------------
     |
     | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be c
+    | an array of paths that should be checked for your views. Of course
+    | the usual Laravel view path has already been registered for you.
+    |
+    */
+
+    'paths' => [
+        resource_path('views'),
