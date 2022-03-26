201b269721ecd7af75345f8bdf481cd3f490c1cf
moonhater <109796598+moonhater@users.noreply.github.com>
2022-03-26 09:31:12 +0000

    Refactor test

--- database/factories/RetailerFactory.php
+++ database/factories/RetailerFactory.php
@@ -1,3 +1,9 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Fact
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Retailer;
+use Faker\Generator as Faker;
+
+$factory->define(Retailer::class, function (Faker $faker) {
+  
