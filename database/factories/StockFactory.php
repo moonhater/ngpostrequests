04f3eb497a6f364ecb58406a564498738ffadbff
moonhater <109796598+moonhater@users.noreply.github.com>
2022-03-27 13:58:19 +0000

    just in case

--- database/factories/StockFactory.php
+++ database/factories/StockFactory.php
@@ -3,4 +3,10 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Stock;
-use Faker\Generator as Fa
+use Faker\Generator as Faker;
+
+$factory->define(Stock::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
