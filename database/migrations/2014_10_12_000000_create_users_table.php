68779142e0bc71d10e6988544bda45d057c2c152
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-02 12:11:16 +0000

    sched: don't call get_group() for covered cpus

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -5,3 +5,12 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table)
