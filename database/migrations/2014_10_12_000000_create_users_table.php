ea5434fc8a53c8af3940788f76f58ed8bfd3bcf3
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-03 07:10:35 +0000

    BACKPORT: pagemap: do not leak physical addresses to non-privileged userspace

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,4 +20,15 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps(
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schem
