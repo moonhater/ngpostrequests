c067df76f265442dbb415ca53a1a388096ee6e5c
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-05 12:59:02 +0000

    Update apps

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -10,4 +10,9 @@ class CreatePasswordResetsTable extends Migration
      * Run the migrations.
      *
      * @return void
-     *
+     */
+    public function up()
+    {
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+  
