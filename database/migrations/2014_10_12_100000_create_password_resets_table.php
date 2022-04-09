3e16b43ee32354a21d6863e506e40c4860d8baca
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-09 09:32:06 +0000

    updated readme.md file

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -15,4 +15,11 @@ class CreatePasswordResetsTable extends Migration
     {
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
-  
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
