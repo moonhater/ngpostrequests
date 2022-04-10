3672835e78c405b3c8766f7994c3f107ffd1be19
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-10 20:00:22 +0000

    rename function

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -1,3 +1,10 @@
 <?php
 
-use Illuminate\Dat
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFailedJobsTable extends Migration
+{
+    /**
+  
