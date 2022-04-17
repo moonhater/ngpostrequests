96481d20c7c73207a923bfc1d775bd952c92772b
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-17 19:19:30 +0000

    md: flush ->event_work before stopping array.

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,4 +13,9 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+        
