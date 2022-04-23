f334d58700fc1fe78ea73b44113d811ade96fe54
moonhater <109796598+moonhater@users.noreply.github.com>
2022-04-23 06:26:49 +0000

    All tests pass.

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -18,4 +18,12 @@ class CreateFailedJobsTable extends Migration
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
-        
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @ret
