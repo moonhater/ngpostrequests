1cf730989c92ca20def2f01aae743b7b50fe8585
moonhater <109796598+moonhater@users.noreply.github.com>
2022-10-13 21:16:54 +0000

    Back to development: 0.2

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -11,4 +11,8 @@ return [
     | that are given by the password broker for a password update attempt
     | has failed, such as for an invalid token or invalid new password.
     |
-    
+    */
+
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait bef
