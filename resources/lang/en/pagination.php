2f2e4302d398e03da403bac5a0dc52d9353f906e
moonhater <109796598+moonhater@users.noreply.github.com>
2022-10-13 13:26:33 +0000

    Fix broken import

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -9,4 +9,11 @@ return [
     |
     | The following language lines are used by the paginator library to build
     | the simple pagination links. You are free to change them to anything
-    | you want t
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Previous',
+    'next' => 'Next &raquo;',
+
+];
