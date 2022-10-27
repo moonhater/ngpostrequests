078c670b8dfb588651d185399f4b6551f92357f0
moonhater <109796598+moonhater@users.noreply.github.com>
2022-10-27 06:04:02 +0000

    25

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -8,4 +8,9 @@ return [
     |--------------------------------------------------------------------------
     |
     | The following language lines contain the default error messages used by
-    | the validator class. So
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    '
