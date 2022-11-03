9a98bb356dd818c277387f61f57cea562fecba12
moonhater <109796598+moonhater@users.noreply.github.com>
2022-11-03 12:49:02 +0000

    Z

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -25,3 +25,5 @@ return [
     'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
     'between' => [
         'numeric' => 'The :attribute must be between :min and :max.',
+        'file' => 'The :attribute must be between :min and :max kilobytes.',
+        'string' => 'The :attribute must be betwee
