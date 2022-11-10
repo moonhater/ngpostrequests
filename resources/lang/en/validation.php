05f09372118985ad43809fd2846c904c012188aa
moonhater <109796598+moonhater@users.noreply.github.com>
2022-11-10 18:53:28 +0000

    0.3.12

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -38,4 +38,7 @@ return [
     'digits' => 'The :attribute must be :digits digits.',
     'digits_between' => 'The :attribute must be between :min and :max digits.',
     'dimensions' => 'The :attribute has invalid image dimensions.',
-    'distinct' => 'The :attribute field has a duplicate valu
+    'distinct' => 'The :attribute field has a duplicate value.',
+    'email' => 'The :attribute must be a valid email address.',
+    'ends_with' => 'The :attribute must end with one of the following: :values.',
+    'exists'
