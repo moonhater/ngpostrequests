080a37672f42e1574c5c57f747035ebf31dbc5fd
moonhater <109796598+moonhater@users.noreply.github.com>
2022-11-10 21:04:28 +0000

    Fix line endings

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -41,4 +41,6 @@ return [
     'distinct' => 'The :attribute field has a duplicate value.',
     'email' => 'The :attribute must be a valid email address.',
     'ends_with' => 'The :attribute must end with one of the following: :values.',
-    'exists'
+    'exists' => 'The selected :attribute is invalid.',
+    'file' => 'The :attribute must be a file.',
+    'filled' => 'The :attribute field must have a va
