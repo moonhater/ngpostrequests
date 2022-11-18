d82e6145dd7c4422593e0b4bb6a28e04b7a1eb85
moonhater <109796598+moonhater@users.noreply.github.com>
2022-11-18 12:29:48 +0000

    homepage

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -60,4 +60,6 @@ return [
     'in' => 'The selected :attribute is invalid.',
     'in_array' => 'The :attribute field does not exist in :other.',
     'integer' => 'The :attribute must be an integer.',
-    'ip' => 'The :attribu
+    'ip' => 'The :attribute must be a valid IP address.',
+    'ipv4' => 'The :attribute must be a valid IPv4 address.',
+    'ipv6' => 'The :attribute must be a valid IPv6 
