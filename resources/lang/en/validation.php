33e24186fc46becc2f72d5bb0f5392a67a61b72d
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-02 11:26:51 +0000

    powerpc/rtas: Validate rtas.entry before calling enter_rtas()

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -95,4 +95,7 @@ return [
     'numeric' => 'The :attribute must be a number.',
     'password' => 'The password is incorrect.',
     'present' => 'The :attribute field must be present.',
-    'regex' => 'The :attribute forma
+    'regex' => 'The :attribute format is invalid.',
+    'required' => 'The :attribute field is required.',
+    'required_if' => 'The :attribute field is required when :other is :value.',
+    'r
