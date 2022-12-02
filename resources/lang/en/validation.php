69abbb9a70827f54e26b3e397d5f39d22f53af8b
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-02 12:49:32 +0000

    new fix

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -101,4 +101,5 @@ return [
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
     'required_with_all' => 'The :attribute field is required when :values are present.',
-    'req
+    'required_without' => 'The :attribute field is required when :values is not present.',
+    'required_without_all' => 'The :attribute field is required
