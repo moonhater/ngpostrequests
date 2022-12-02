99a91c0c1cc4e3d6674d6da06c90b970ed9262d1
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-02 18:02:11 +0000

    Add PHPDoc

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -105,4 +105,7 @@ return [
     'required_without_all' => 'The :attribute field is required when none of :values are present.',
     'same' => 'The :attribute and :other must match.',
     'size' => [
-        'numeric' => 'The :attribute must b
+        'numeric' => 'The :attribute must be :size.',
+        'file' => 'The :attribute must be :size kilobytes.',
+        'string' => 'The :attribute must be :size characters.',
+        'array' => 'Th
