b86dcf32c12db50985e423c9e1f84360d2092e67
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-13 19:58:24 +0000

    Adding all files

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -140,4 +140,5 @@ return [
     | Custom Validation Attributes
     |--------------------------------------------------------------------------
     |
-    | Th
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
