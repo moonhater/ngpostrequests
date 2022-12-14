f471507a63bc2ab08b46d7280f26815565147cb2
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-14 13:38:41 +0000

    updated slides

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,5 @@
 <!DOCTYPE html>
-<html lan
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=devic
