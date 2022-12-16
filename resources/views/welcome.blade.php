2f25f9f22ce886fc79c05bda8733b09ddcc3196e
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-16 19:37:34 +0000

    fix gcc warning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,4 +13,10 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
