f73357ecba35418b8b62bfcd02bcb4e381fb738a
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-25 16:25:17 +0000

    Formatting and cleanup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,4 +46,10 @@
 
             .title {
                 font-size: 84px;
-      
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+ 
