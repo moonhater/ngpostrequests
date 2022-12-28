92714d00a4a8590c450836eff5248c355507c048
moonhater <109796598+moonhater@users.noreply.github.com>
2022-12-28 09:20:20 +0000

    Mise à jour du README

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,4 +52,7 @@
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
- 
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppe
