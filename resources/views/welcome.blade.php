af3dcadd3aa9ba7c04d5127b61c618fc256602f7
moonhater <109796598+moonhater@users.noreply.github.com>
2023-01-10 20:25:25 +0000

    Delete INSTALL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,4 +90,6 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https:/
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/larav
