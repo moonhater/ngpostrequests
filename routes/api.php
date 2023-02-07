ea8d0eef548881cdd37f155ee4033d6f35f5917d
moonhater <109796598+moonhater@users.noreply.github.com>
2023-02-07 18:10:18 +0000

    add timezone

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
-|----
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded
