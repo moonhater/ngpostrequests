ece31772718443d36b4f9e5bb202a609f8740a1c
moonhater <109796598+moonhater@users.noreply.github.com>
2023-02-07 07:05:14 +0000

    Updated Travis CI config.

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
-use Illuminate\Http\Requ
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|----
