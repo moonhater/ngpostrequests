07043a458f7782f8744235fde3de724afca0f708
moonhater <109796598+moonhater@users.noreply.github.com>
2023-02-09 10:45:09 +0000

    improve styles

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,9 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:a
