fea324ce5dbc4667f8e8fec6ae6e19ed24713e52
moonhater <109796598+moonhater@users.noreply.github.com>
2023-02-28 17:58:18 +0000

    add a readme

--- routes/channels.php
+++ routes/channels.php
@@ -9,4 +9,9 @@ use Illuminate\Support\Facades\Broadcast;
 |
 | Here you may register all of the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
-| used to check if a
+| used to check if an authenticated user can listen to the channel.
+|
+*/
+
+Broadcast::channel('App.User.{id}', function ($user, $id) {
+    return (int) $user->id =
