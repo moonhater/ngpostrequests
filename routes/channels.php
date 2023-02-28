c16d113b6e6351ff379111301471a213386bf0e5
moonhater <109796598+moonhater@users.noreply.github.com>
2023-02-28 14:30:33 +0000

    Fix missing import.

--- routes/channels.php
+++ routes/channels.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
-| Here you may registe
+| Here you may register all of the event broadcasting channels that your
+| application supports. The given channel authorization callbacks are
+| used to check if a
