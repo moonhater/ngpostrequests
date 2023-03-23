c8221802597a41f008340744671afe2bba836422
moonhater <109796598+moonhater@users.noreply.github.com>
2023-03-23 14:16:50 +0000

    Fix specs.

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
+|--------------------------------------------------------------------------
 |
+| Here is where you can register web routes for your application. These
+| routes are loaded by th
