600e27cfe00b3b801e7cf2c738b53c80141432b1
moonhater <109796598+moonhater@users.noreply.github.com>
2023-04-04 15:47:50 +0000

    chore: Update changelog

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,4 +5,6 @@ const mix = require('laravel-mix');
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
- | Mix provides
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for t
